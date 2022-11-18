b3612413c37f400ac37e38cb56f19d32e56d4f86
windmgc <windmgc@gmail.com>
2022-11-18 15:34:18 +0800

    let sam local run on background

--- spec/fixtures/aws-sam.lua
+++ spec/fixtures/aws-sam.lua
@@ -37,7 +37,7 @@ function _M.start_local_lambda()
     return nil, "No available port found"
   end
 
-  local ret, err = utils.execute("sam local start-lambda --template-file=spec/fixtures/sam-app/template.yaml --port " .. port)
+  local ret, err = utils.wait_output("sam local start-lambda --template-file=spec/fixtures/sam-app/template.yaml --port " .. port, "Starting the Local Lambda Service", 30)
   if err then
     return nil, fmt("Start SAM CLI failed(code: %s): %s", err, ret)
   end
